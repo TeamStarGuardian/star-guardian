90507f151c6b244be342dd150a5895a157d8d817
awhite747 <awhite747@gmail.com>
2016-03-24 13:35:19 -0400

    git push test 2
    modified .gitignore file in attempt to keep files when pushing

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour
+{
+	private bool facingRight = false;
+	public float speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			if (!facingRight)
+			{
+				flip();
+			}
+			//move right
+		}
+		else if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			if (facingRight)
+			{
+				flip();
+			}
+			//move left
+		}
+	}
+
+	void flip()
+	{
+		facingRight = !facingRight;
+
+		Vector2 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+}
