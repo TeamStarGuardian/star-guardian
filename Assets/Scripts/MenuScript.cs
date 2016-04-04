5edf310ecd1da81a8aef883f72c817dea8c75e07
Michael Lu <michaeldelu@gmail.com>
2016-04-04 10:14:07 -0400

    main menu changes, sounds, visuals (all temp, subject to change), exit sub-menu, play button (buggy)). TODO options menu, help menu, pause menu, end-game menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public Canvas quitMenu;
+    public Button playBtn;
+    public Button exitBtn;
+
+    // Use this for initialization
+    void Start()
+    {
+        // assign variables to game objects
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        playBtn = playBtn.GetComponent<Button>();
+        exitBtn = playBtn.GetComponent<Button>();
+        quitMenu.enabled = false;
+    }
+
+    // Show exit submenu and disable the buttons
+    public void ExitButtonPressed()
+    {
+        quitMenu.enabled = true;
+        playBtn.enabled = false;
+        exitBtn.enabled = false;
+    }
+
+    // Hide exit submenu and re-enable the buttons
+    public void ExitMenuNoPressed()
+    {
+        quitMenu.enabled = false;
+        playBtn.enabled = true;
+        exitBtn.enabled = true;
+    }
+
+    public void PlayButtonPressed()
+    {
+        SceneManager.LoadScene("Play");
+        //string sceneName = SceneManager.GetActiveScene().name;
+        //SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    // Exit the game
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
