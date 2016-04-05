18ba8ee8647a0d3d58c5269d313b9b5b1eb0d3f7
Michael Lu <michaeldelu@gmail.com>
2016-04-05 01:29:45 -0400

    update with working options screen and credits screens, TODO help screen, pause screen, end game screen

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -8,6 +8,7 @@ public class MenuScript : MonoBehaviour
     // menus
     public Canvas startMenu;
     public Canvas quitMenu;
+    public Canvas creditsMenu;
     public Canvas optionsMenu;
     public Canvas helpMenu;
 
@@ -18,6 +19,15 @@ public class MenuScript : MonoBehaviour
     public Button helpBtn;
     public Button backBtnOptions;
     public Button backBtnHelp;
+    public Button musicBtn;
+    public Button difficultyBtn;
+    public Button closeBtn;
+    public Button creditsBtn;
+
+    // music and difficulty
+    public AudioSource music;
+    private bool musicEnabled;
+    private bool isEasy;
 
     // Use this for initialization
     void Start()
@@ -26,6 +36,7 @@ public class MenuScript : MonoBehaviour
         startMenu = startMenu.GetComponent<Canvas>();
         quitMenu = quitMenu.GetComponent<Canvas>();
         optionsMenu = optionsMenu.GetComponent<Canvas>();
+        creditsMenu = creditsMenu.GetComponent<Canvas>();
         helpMenu = helpMenu.GetComponent<Canvas>();
 
         playBtn = playBtn.GetComponent<Button>();
@@ -34,12 +45,23 @@ public class MenuScript : MonoBehaviour
         helpBtn = helpBtn.GetComponent<Button>();
         backBtnOptions = backBtnOptions.GetComponent<Button>();
         backBtnHelp = backBtnHelp.GetComponent<Button>();
+        musicBtn = musicBtn.GetComponent<Button>();
+        difficultyBtn = difficultyBtn.GetComponent<Button>();
+        creditsBtn = creditsBtn.GetComponent<Button>();
+        closeBtn = closeBtn.GetComponent<Button>();
+
+        music = music.GetComponent<AudioSource>();
 
         // instantiate non-start menus to false
         startMenu.enabled = true;
         optionsMenu.enabled = false;
         quitMenu.enabled = false;
         helpMenu.enabled = false;
+        creditsMenu.enabled = false;
+
+        // set booleans for music and diff
+        musicEnabled = true;
+        isEasy = true;
     }
 
     // Show exit submenu and disable the buttons
@@ -75,6 +97,24 @@ public class MenuScript : MonoBehaviour
         optionsMenu.enabled = true;
     }
 
+    // Display the credits screen, disable other buttons
+    public void CreditsButtonPressed()
+    {
+        creditsMenu.enabled = true;
+        backBtnOptions.enabled = false;
+        musicBtn.enabled = false;
+        difficultyBtn.enabled = false;
+    }
+    
+    // Hide the credits screen, re-enable the other buttons
+    public void CloseButtonPressed()
+    {
+        creditsMenu.enabled = false;
+        backBtnOptions.enabled = true;
+        musicBtn.enabled = true;
+        difficultyBtn.enabled = true;
+    }
+
     // Show the help menu by hiding others
     public void HelpButtonPressed()
     {
@@ -88,6 +128,39 @@ public class MenuScript : MonoBehaviour
         startMenu.enabled = true;
         helpMenu.enabled = false;
         optionsMenu.enabled = false;
+    }
+
+    // Toggle the music
+    public void MusicButtonPressed()
+    {
+        if (musicEnabled)
+        {
+            musicEnabled = false;
+            music.enabled = false;
+            music.Stop();
+            musicBtn.GetComponentInChildren<Text>().text = "turn music on";
+            // toggle comment
+        } else
+        {
+            musicEnabled = true;
+            music.enabled = true;
+            music.Play();
+            musicBtn.GetComponentInChildren<Text>().text = "turn music off";
+        }
+    }
+
+    // Toggle the difficulty
+    public void DifficultyButtonPressed()
+    {
+        if (isEasy)
+        {
+            isEasy = false;
+            difficultyBtn.GetComponentInChildren<Text>().text = "set difficulty: easy";
+        } else
+        {
+            isEasy = true;
+            difficultyBtn.GetComponentInChildren<Text>().text = "set difficulty: hard";
+        }
     }
 
     // Exit the game
